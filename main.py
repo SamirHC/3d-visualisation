fe1901d3dc9083442a1230cd7617fba140554f31
Samir <cyberdragon1184@gmail.com>
2020-08-26 17:46:34 +0100

    Dot function returns a zero matrix of the appropriate dimensions

--- main.py
+++ main.py
@@ -13,18 +13,20 @@ p.display.set_caption(CAPTION)
 
 # Matrix functions
 def Dot(matrix1, matrix2):
-    if np.shape(matrix1)[0] != np.shape(matrix2)[1]:
-        raise ValueError("You cannot dot product matrices where the first matrix row is of different dimension to the second matrix column")
-    
+    matrix1_rows, matrix1_columns = np.shape(matrix1)
+    matrix2_rows, matrix2_columns = np.shape(matrix2)
+    if matrix1_columns != matrix2_rows:
+        raise ValueError("You cannot dot product matrices where the first matrix column is of different dimension to the second matrix row")
+    new_matrix = np.zeros((matrix1_rows, matrix2_columns))  # Creates an empty new_matrix which we will store the result in
+    return new_matrix
 #Testing
 a = np.array([
     [1, 2],
     [2, 4],
-    #[4, 5]
+    [4, 5]
 ])
 b = np.array([
     [1, 4, 3],
     [2, 5, 4],
 ])
-
-Dot(a, b)
+print(Dot(a, b))
