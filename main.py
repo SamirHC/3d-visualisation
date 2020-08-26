ad970857d0273e0d69efbe5fe65f955debb7c6d1
Samir <cyberdragon1184@gmail.com>
2020-08-26 22:22:44 +0100

    Added the function intersectionOfLineAndPlane which returns the coordinates where a line intersects a plane

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#import pygame as p
+import pygame as p
 import math
 import numpy as np
 
-#p.init()
+p.init()
 
 # Display Settings
 display_width = 800
 display_height = 600
-# display = p.display.set_mode((display_width, display_height))
+display = p.display.set_mode((display_width, display_height))
 CAPTION = "3d"
-# p.display.set_caption(CAPTION)
+p.display.set_caption(CAPTION)
 
 # Matrix functions
 def Dot(matrix1, matrix2):  # Calculates the dot product of two matrices
@@ -32,15 +32,28 @@ def Cross(matrix1, matrix2):  # Calculates the cross product of two (3, 1) matri
 def unitVector(vector):
     return vector / np.linalg.norm(vector)
 
-def angleBetweenVectors(v1, v2):
+def angleBetweenVectors(v1, v2):  # Returns angle between vectors measured in radians
     v1_u = unitVector(v1)
     v2_u = unitVector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
+def intersectionOfLineAndPlane(lineData, planeData):  # Returns the point (if any) of intersection between a line and a plane
+    # lineData is in the form :: [p, v] where p is the point and v is a vector, given as np.arrays
+    # planeData is in the form :: [p, n] as above, but n is the normal vector to the plane
+    V0 = planeData[0]
+    P0 = lineData[0]
+    n = planeData[1]
+    u = lineData[1]
+    if np.dot(n, u) == 0:
+        return np.array([256, 256, 256])
+    w = P0 - V0
+    s = -np.dot(n, w) / np.dot(n, u)  # Calculates the value of the parametric variable
+    return P0 + s*u
+    
 # Camera
 camera_position = np.array([0, 0, 0])  # Initially at the origin, position given as a coordinate
 camera_direction = np.array([1, 0, 0])  # Initially looking parallel to the positive x-axis, direction is given as a unit vector
-
+camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in.
 #Objects
 class Triangle:
     def __init__(self, v1, v2, v3):  # Vertices of the triangle, (should be np.array of shape (3, 1))
@@ -54,15 +67,16 @@ class Triangle:
 
 #Testing
 triangles = []
-tri1 = Triangle(np.array([1, 2, 3]), np.array([0, 0, 1]), np.array([4, 1, 2]))
+tri1 = Triangle(np.array([1, 2, 3]), np.array([8, 2, 1]), np.array([4, 1, 2]))
 triangles.append(tri1)
 
 #Rendering
 for triangle in triangles:
     vertices = [triangle.v1, triangle.v2, triangle.v3]
-    vectors_to_camera = []
+    intersections = []
     for vertex in vertices:
-        vectors_to_camera.append(camera_position - vertex)
+        line_to_camera = [vertex, camera_position - vertex]
+        intersection = intersectionOfLineAndPlane(line_to_camera, camera_screen)
+        print(intersection)
 
-for vector in vectors_to_camera:
-    pass
+
