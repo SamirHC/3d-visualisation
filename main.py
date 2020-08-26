8316aa1dd873d9e20bf7186f18ce6ed2c5bbb440
Samir <cyberdragon1184@gmail.com>
2020-08-26 16:46:33 +0100

    Created a 800x600 display with python's pygame library

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame as p
+import math
+
+p.init()
+
+# Display Settings
+display_width = 800
+display_height = 600
+display = p.display.set_mode((display_width, display_height))
+CAPTION = "3d"
+p.display.set_caption(CAPTION)
