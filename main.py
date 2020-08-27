2a8b14b7de90b226c37dc1c1c81d7a12c5a36cfc
Samir <cyberdragon1184@gmail.com>
2020-08-27 13:01:52 +0100

    Added a getAxisMatrix function which calculates the combined matrix required to rotate a plane in 3D space. Also added a corresponding getInverseAxisMatrix function which returns the inverse (using numpy's np.linalg.inv() function).

--- main.py
+++ main.py
@@ -16,7 +16,15 @@ p.display.set_caption(CAPTION)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Vectors
+# Initial Camera Settings
+alpha = 0  # Angle in the xy plane
+beta = 0 # Angle in the xz plane
+gamma = 0  # Angle in the yz plane
+x0 = 0  # x coordinate of camera
+y0 = 0  # y coordinate of camera
+z0 = 0  # z coordinate of camera
+
+# Vector Calculations
 def unitVector(vector):
     return vector / np.linalg.norm(vector)
 
@@ -39,17 +47,34 @@ def intersectionOfLineAndPlane(lineData, planeData):  # Returns the point (if an
     return P0 + s*u
     
 # Camera
+def getAxisMatrix(ALPHA, BETA, GAMMA):
+    alpha_rotation_matrix = np.array([
+        [math.cos(ALPHA), -math.sin(ALPHA), 0],
+        [math.sin(ALPHA), math.cos(ALPHA), 0],
+        [0, 0, 1]
+        ])  # Matrix required to rotate a plane by ALPHA in the xy plane
+    beta_rotation_matrix = np.array([
+        [math.cos(BETA), 0, -math.sin(BETA)],
+        [0, 1, 0],
+        [math.sin(BETA), 0, math.cos(BETA)]
+        ])  # Matrix required to rotate a plane by ALPHA in the xz plane
+    gamma-rotation_matric = np.array([
+        [1, 0, 0],
+        [0, math.cos(GAMMA), -math.sin(GAMMA)],
+        [0, math.sin(GAMMA), math.cos(GAMMA)]
+        ])  # Matrix required to rotate a plane by ALPHA in the yz plane
+    axisMatrix = np.dot(np.dot(alpha_rotation_matrix, beta_rotation_matrix), gamma_rotation_matrix)
+        
 def findCameraDirection(ALPHA, BETA, GAMMA):
     i = math.sin(ALPHA)*math.sin(GAMMA) - math.cos(ALPHA)*math.sin(BETA)*math.cos(GAMMA)
     j = -math.cos(ALPHA)*math.sin(GAMMA) - math.sin(ALPHA)*math.sin(BETA)*math.cos(GAMMA)
     k = math.cos(BETA)*math.cos(GAMMA)
     return np.array([i, j, k])
-alpha = 0  # Angle in the xy plane
-beta = 0  # Angle in the xz plane
-gamma = 0  # Angle in the yz plane
-x0 = 0  # x coordinate of camera
-y0 = 0  # y coordinate of camera
-z0 = 0  # z coordinate of camera
+
+def getInverseAxisMatrix(ALPHA, BETA, GAMMA):
+    inverseMatrix = np.linalg.inv(getAxisMatrix(ALPHA, BETA, GAMMA))
+    return inverseMatrix
+        
 camera_position = np.array([x0, y0, z0])  # Initially at the origin, position given as a coordinate np.array
 camera_direction = findCameraDirection(alpha, beta, gamma)  # Initially looking parallel to the positive x-axis, direction is given as a unit vector np.array
 camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in [point, normal unit vector]
@@ -78,11 +103,11 @@ triangles.append(tri4)
 print(camera_direction)
 
 """
+#Rendering
 while True:
     display.fill(BLACK)
     camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in.
 
-    #Rendering
     for triangle in triangles:
         vertices = [triangle.v1, triangle.v2, triangle.v3]
         intersections = []
