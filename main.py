465050a1e99911692ff4239f01a735d739999302
Samir Hasan Chowdhury <samir.hasanchowdhury@gmail.com>
2024-08-06 17:05:04 +0100

    pygbag compatible changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import pygame as p
 import math
 import numpy as np
@@ -25,14 +26,6 @@ GRAY = (128, 128, 128)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
-# Initial Camera Settings
-alpha = 0.  # Angle in the xy plane
-beta = 0.  # Angle in the xz plane
-gamma = 0.  # Angle in the yz plane
-x0 = 0.  # x coordinate of camera
-y0 = 0.  # y coordinate of camera
-z0 = 0.  # z coordinate of camera
 
 # Vector Calculations
 def unitVector(vector):
@@ -93,21 +86,13 @@ def getAnglesFromMousePosition():
     GAMMA = y/display_height
     return np.array([ALPHA, BETA, GAMMA])
 
-def getRelativeAxes(key):
+def getRelativeAxes(key, camera_direction):
     axesDict = {}
     axesDict["forward"] = -camera_direction
     axesDict["forward"][1] = 0.
     axesDict["side"] = np.cross(camera_direction, np.array([0, 1, 0]))
     return axesDict[key]
-    
 
-axisMatrix = getAxisMatrix(alpha, beta, gamma)  # The orientation of the xyz axis relative to the camera
-inverseAxisMatrix = np.linalg.inv(axisMatrix)  # The inverse of the above,  used in order to map back to the display
-camera_position = np.array([x0, y0, z0])  # Initially at the origin, position given as a coordinate np.array
-camera_direction = findCameraDirection(alpha, beta, gamma)  # Direction is given as a unit vector np.array and is the normal to the camera_screen
-camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in [point, normal unit vector]
-shift = np.array([0.5, 0.5, 0])
-scale = np.array([display_width, display_width, 0])
 
 #Objects
 class Shape:
@@ -119,38 +104,38 @@ class Shape:
     def center(self):
         return np.sum(self.vertices, axis=0)/len(self.vertices)
 
-    def map_point_to_screen(self, point):
+    def map_point_to_screen(self, point, camera_position, camera_screen, inverseAxisMatrix, shift, scale):
         line_to_camera = [point, camera_position - point]
         intersection_point = intersectionOfLineAndPlane(line_to_camera, camera_screen)
         mapped_point = ((np.dot(inverseAxisMatrix, intersection_point - camera_position)+shift)*scale)[:-1]
         return mapped_point
 
-    def map_vertices(self):
+    def map_vertices(self, camera_position, camera_screen, inverseAxisMatrix, shift, scale):
         self.mapped_vertices = []
         for vertex in self.vertices:
-            self.mapped_vertices.append(self.map_point_to_screen(vertex))
+            self.mapped_vertices.append(self.map_point_to_screen(vertex, camera_position, camera_screen, inverseAxisMatrix, shift, scale))
         return self.mapped_vertices
 
-    def draw(self):
-        for mapped_vertex in self.map_vertices():
+    def draw(self, camera_position, camera_screen, inverseAxisMatrix, shift, scale):
+        for mapped_vertex in self.map_vertices(camera_position, camera_screen, inverseAxisMatrix, shift, scale):
             if np.linalg.norm(mapped_vertex) > display_width**2:  # Coordinates can't be too extreme
                 return
         self.draw_method()
 
     def draw_method(self):
-        p.draw.polygon(display, shape.color, np.rint(self.mapped_vertices))
+        p.draw.polygon(display, self.color, np.rint(self.mapped_vertices))
 
-    def vector_from_camera(self):
+    def vector_from_camera(self, camera_position):
         return self.center - camera_position
 
-    def distance_from_camera(self):
-        return np.linalg.norm(self.vector_from_camera())*self.facing()
+    def distance_from_camera(self, camera_position, camera_direction):
+        return np.linalg.norm(self.vector_from_camera(camera_position))*self.facing(camera_position, camera_direction)
 
-    def facing(self):
-        return -np.sign(np.dot(self.vector_from_camera(), camera_direction))
+    def facing(self, camera_position, camera_direction):
+        return -np.sign(np.dot(self.vector_from_camera(camera_position), camera_direction))
 
-    def is_front(self):
-        return True if self.facing() == 1 else False
+    def is_front(self, camera_position, camera_direction):
+        return True if self.facing(camera_position, camera_direction) == 1 else False
 
 class Triangle(Shape):
     def __init__(self, vertices, color=WHITE):  # Vertices of the triangle, (should be np.array of shape (3, 1))
@@ -198,52 +183,74 @@ shapes.append(tri2)
 ##shapes.append(tri2)
 
 # Run
-running = True
-t0= time.time()
-while running:
-    # Rendering
-    display.fill(BLACK)
-    camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in.
-    axisMatrix = getAxisMatrix(alpha, beta, gamma)
-    inverseAxisMatrix = np.linalg.inv(axisMatrix)
-    shapes.sort(key=lambda x: x.distance_from_camera(), reverse=True)
-    for shape in shapes:
-        if shape.is_front():
-            shape.draw()
-        else:
-            break
-    display.blit(p.transform.flip(display, False, False), (0, 0))
-    p.display.update()
-    # Controls
-    keys = p.key.get_pressed()
-    forward_speed, side_speed = (0, 0)
-    if keys[p.K_a]:
-        side_speed = -3
-    elif keys[p.K_d]:
-        side_speed = 3
-    if keys[p.K_w]:
-        forward_speed = 3
-    elif keys[p.K_s]:
-        forward_speed = -3
-    if keys[p.K_p]:
-        print(camera_position)
-        print(camera_direction)
-    # Animate
-    t1 = time.time()
-    dt = t1 - t0
-    t0 = t1
-    delta_angles = getAnglesFromMousePosition()
-    alpha += delta_angles[0]
-    beta += delta_angles[1]
-    gamma += delta_angles[2]
-    #beta = -t
-    #gamma = -0.2
-    #camera_position = np.array([5*math.sin(t), 1, 5 + 5*math.cos(t)])
-    camera_direction = findCameraDirection(alpha, beta, gamma)
-    camera_position += forward_speed*dt*getRelativeAxes("forward") + side_speed*dt*getRelativeAxes("side")
-    # Misc
-    for event in p.event.get():
-        if event.type == p.QUIT:
-            running = False
-    clock.tick(FPS)
+async def main():
+    # Initial Camera Settings
+    alpha = 0.  # Angle in the xy plane
+    beta = 0.  # Angle in the xz plane
+    gamma = 0.  # Angle in the yz plane
+    x0 = 0.  # x coordinate of camera
+    y0 = 0.  # y coordinate of camera
+    z0 = 0.  # z coordinate of camera
 
+    axisMatrix = getAxisMatrix(alpha, beta, gamma)  # The orientation of the xyz axis relative to the camera
+    inverseAxisMatrix = np.linalg.inv(axisMatrix)  # The inverse of the above,  used in order to map back to the display
+    camera_position = np.array([x0, y0, z0])  # Initially at the origin, position given as a coordinate np.array
+    camera_direction = findCameraDirection(alpha, beta, gamma)  # Direction is given as a unit vector np.array and is the normal to the camera_screen
+    camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in [point, normal unit vector]
+    shift = np.array([0.5, 0.5, 0])
+    scale = np.array([display_width, display_width, 0])
+
+    running = True
+    t0= time.time()
+    while running:
+        # Rendering
+        display.fill(BLACK)
+        camera_screen = [camera_position + camera_direction, camera_direction]  # Represents the plane the display is in.
+        axisMatrix = getAxisMatrix(alpha, beta, gamma)
+        inverseAxisMatrix = np.linalg.inv(axisMatrix)
+        shapes.sort(key=lambda x: x.distance_from_camera(camera_position, camera_direction), reverse=True)
+        for shape in shapes:
+            if shape.is_front(camera_position, camera_direction):
+                shape.draw(camera_position, camera_screen, inverseAxisMatrix, shift, scale)
+            else:
+                break
+        display.blit(p.transform.flip(display, False, False), (0, 0))
+        p.display.update()
+        # Controls
+        keys = p.key.get_pressed()
+        forward_speed, side_speed = (0, 0)
+        if keys[p.K_a]:
+            side_speed = -3
+        elif keys[p.K_d]:
+            side_speed = 3
+        if keys[p.K_w]:
+            forward_speed = 3
+        elif keys[p.K_s]:
+            forward_speed = -3
+        elif keys[p.K_p]:
+            print(camera_position)
+            print(camera_direction)
+        elif keys[p.K_ESCAPE]:
+            p.event.post(p.QUIT)
+        # Animate
+        t1 = time.time()
+        dt = t1 - t0
+        t0 = t1
+        delta_angles = getAnglesFromMousePosition()
+        alpha += delta_angles[0]
+        beta += delta_angles[1]
+        gamma += delta_angles[2]
+        #beta = -t
+        #gamma = -0.2
+        #camera_position = np.array([5*math.sin(t), 1, 5 + 5*math.cos(t)])
+        camera_direction = findCameraDirection(alpha, beta, gamma)
+        camera_position += forward_speed*dt*getRelativeAxes("forward", camera_direction) + side_speed*dt*getRelativeAxes("side", camera_direction)
+        # Misc
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+        
+        clock.tick(FPS)
+        await asyncio.sleep(0)
+
+asyncio.run(main())
